64fbe5402b71851cf640749055f320661f22b4eb
k1nn13 <58257145+k1nn13@users.noreply.github.com>
2023-03-18 17:41:21 +0000

    Added game loop UI

--- XRCP_VR/Assets/Activities_XRCP/GroupProjectTemplate/Scripts/TerminalScripts/ComputerUI_Controller.cs
+++ XRCP_VR/Assets/Activities_XRCP/GroupProjectTemplate/Scripts/TerminalScripts/ComputerUI_Controller.cs
@@ -10,42 +10,60 @@ public class ComputerUI_Controller : MonoBehaviour
     [Header("Object Reference")]
     [SerializeField] GameObject pTube;
     [SerializeField] GameObject socketOBJ;
+    [SerializeField] GameObject socketOutOBJ;
 
     [Header("UI Component")]
-    [SerializeField] Button btn_0;
-    [SerializeField] Slider slider_0;
+
     [SerializeField] GameObject tmpRecievedOBJ;
+    [SerializeField] GameObject tmpSentOBJ;
     [SerializeField] GameObject inputDetectedOBJ;
     [SerializeField] GameObject messageLoggedOBJ;
+    [SerializeField] GameObject messageOutCanvas;
+    [SerializeField] Button btn_0, logBtn, launchBtn;
+    [SerializeField] TMP_Text warningText, launchText;
+    [SerializeField] Transform current, start, end;
 
     [Header("Public Values")]
     public float sliderValue = 0;
-    public bool socketStateNow;
+    public bool socketStateNow, socketOutState;
 
     ////------------------------ Update UI
     dynamicText dt;
     TubeSpawner ts;
     TextMeshProUGUI textRecieved;
+    TextMeshProUGUI textSent;
     TextMeshProUGUI inputDetected;
     TextMeshProUGUI messageLogged;
     ColorBlock cb;
+    int tubeCount;
+    int tubeCountSent;
+    Rigidbody rb;
 
-    //------------------------ Socket Test
+    //------------------------ Socket IN
     XRSocketInteractor socket;
     GameObject tubeMessage;
     TubeLog tubeLog;
+
+    //------------------------ Socket Out
+    XRSocketInteractor socketOut;
+    GameObject tubeMessageOut;
+    TubeLog tubeLogOut;
 
     //---------------
     void Start()
     {
         // Access TMP variables on Game Object
         textRecieved = tmpRecievedOBJ.GetComponent<TextMeshProUGUI>();
+        textSent = tmpSentOBJ.GetComponent<TextMeshProUGUI>();
 
         // Get Access to number of tubes spawned
         ts = pTube.GetComponent<TubeSpawner>();
 
         // Listen to when ui button is pressed
-        btn_0.onClick.AddListener(TaskOnClick);
+        btn_0.onClick.AddListener(TaskOnClick); // main button debugging
+        logBtn.onClick.AddListener(LogMessage);
+        launchBtn.onClick.AddListener(LaunchMessage);
+
 
         // access input detected tmp to change colour
         inputDetected = inputDetectedOBJ.GetComponent<TextMeshProUGUI>();
@@ -57,14 +75,28 @@ public class ComputerUI_Controller : MonoBehaviour
 
         // Check whats in the socket
         socket = socketOBJ.GetComponent<XRSocketInteractor>();
+        socketOut = socketOutOBJ.GetComponent<XRSocketInteractor>();
 
-        // Variables to change text colour
-        cb = slider_0.colors;
-        cb.normalColor = Color.red;
-        slider_0.colors = cb;
+        // keep track of number of tubes
+        tubeCount = 0;
+        tubeCountSent = 0;
+
+        // Display button to update message log on tube
+        logBtn.interactable = false;
+        logBtn.image.color = new Color(0,0,0,0);
+
+        launchBtn.interactable = false;
+        launchBtn.image.color = new Color(0, 0, 0, 0);
+        launchText.color = new Color(0, 0, 0, 0);
+
+        // warning message to prompt user on what to do
+        messageOutCanvas.SetActive(false);
     }
 
-    //------------------------------------
+
+
+
+    //------------------------ Input Socket
     public void socketCheck()
     {
         //--------------------------------------------------------------------
@@ -72,41 +104,6 @@ public class ComputerUI_Controller : MonoBehaviour
         IXRSelectInteractable objName = socket.GetOldestInteractableSelected();
         tubeMessage = objName.transform.gameObject;
         tubeLog = tubeMessage.GetComponent<TubeLog>();
-    }
-
-    //----------------
-    void TaskOnClick()
-    {
-        //------------------------------------------------------------------------------
-        //-- Use for debugging, when ui is pressed a new tube is spawned in tube spawner
-        sliderValue ++;
-
-        if (sliderValue > 1)
-        {
-            sliderValue = 0;
-        }
-
-        slider_0.value = sliderValue;
-
-        //--------------------------------
-        // change colour of text, debugging
-        if (sliderValue == 0)
-        {
-            cb.normalColor = Color.red;
-        }
-        
-        if (sliderValue == 1)
-        {
-            cb.normalColor = Color.green;
-        }
-
-        slider_0.colors = cb;
-
-
-        //----------------------------------------------
-        // update tmp "number of messages recieved"
-        // this will need to work with ministry input!!!!!!!
-        textRecieved.text = (ts.tubeCount + 1).ToString();
     }
 
     //------------------------------------------
@@ -125,25 +122,154 @@ public class ComputerUI_Controller : MonoBehaviour
 
             if (tubeLog != null)
             {
-                if(tubeLog.isLogged)
+                if (tubeLog.isLogged)
                 {
+                    // if message logged change color to green
                     messageLogged.color = new Color(0, 255, 0, 255);
+
+                    // hide button to log message
+                    logBtn.interactable = false;
+                    logBtn.image.color = new Color(255, 255, 255, 0);
+
+                    // display warning message
+                    messageOutCanvas.SetActive(true);
+                    warningText.text = "Remove message from P drive and place under output pipe!";
                 }
 
                 if (!tubeLog.isLogged)
                 {
+                    // displays message red - not logged
                     messageLogged.color = new Color(255, 0, 0, 255);
+
+                    // displays button allowing user to log message
+                    logBtn.interactable = true;
+                    logBtn.image.color = new Color(255, 255, 255, 255);
                 }
             }
-          
+
         }
-        
+
         //-----------------
         if (!socketStateNow)
         {
             // set colour -> Alpha to transparent
             inputDetected.color = new Color(0, 0, 0, 0);
             messageLogged.color = new Color(0, 0, 0, 0);
+
+
+            logBtn.interactable = false;
+            logBtn.image.color = new Color(0, 0, 0, 0);
+
+            messageOutCanvas.SetActive(false);
+        }
+
+        Debug.Log("Update Socket");
+    }
+
+    //-------------------------- Output Socket
+    public void socketOutCheck()
+    {
+        //--------------------------------------------------------------------
+        // Get current message game object in the socket
+        IXRSelectInteractable objName = socketOut.GetOldestInteractableSelected();
+        tubeMessageOut = objName.transform.gameObject;
+        tubeLogOut = tubeMessageOut.GetComponent<TubeLog>();
+        rb = tubeMessageOut.GetComponent<Rigidbody>();
+
+        if (tubeLogOut.isLogged)
+        {
+            launchBtn.interactable = true;
+            launchBtn.image.color = new Color(255, 255, 255, 255);
+            launchText.color = new Color(255, 255, 255, 255);
+
+            // display warning message
+            messageOutCanvas.SetActive(true);
+            warningText.text = "Press the button to send the message";
+        }
+       
+    }
+
+    //------------------------------------
+    public void SocketOutState(bool state)
+    {
+        socketOutState = state;
+
+        if (!socketOutState)
+        {
+            launchBtn.interactable = false;
+            launchBtn.image.color = new Color(255, 255, 255, 0);
+            launchText.color = new Color(255, 255, 255, 0);
+
+            // display warning message
+            messageOutCanvas.SetActive(false);
+            
         }
     }
+
+    //------------------
+    //------------------
+    void LaunchMessage()
+    {
+       
+        // launches tube
+        //Destroy(tubeMessageOut);
+        //current.transform.position = end.transform.position;
+        tubeCountSent++;
+        textSent.text = (tubeCountSent).ToString();
+        StartCoroutine(launchCoroutine());
+
+    }
+
+    //-----------------------------
+    IEnumerator launchCoroutine()
+    {
+
+        
+        Vector3 startPos = start.transform.position;
+        Vector3 endPos = end.transform.position;
+
+        float timeElapsed = 0;
+        float lerpTime = 0.5f;
+
+        while (timeElapsed < lerpTime)
+        {
+            current.transform.position = Vector3.Lerp(startPos, endPos, (timeElapsed / lerpTime));
+
+            timeElapsed += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+
+        }
+        Destroy(tubeMessageOut);
+        current.transform.position = start.transform.position;
+    }
+
+    //------------------
+    void LogMessage()
+    {
+        // updates game objects log state
+        if (tubeLog != null)
+        {
+            if (!tubeLog.isLogged)
+            {
+                tubeLog.isLogged = true;
+                updateSocketState(true);
+            }
+        }
+    }
+
+    //---------------------------
+    public void UpdateTubeCount()
+    {
+        // listen to tube spawn function to update number of tubes spawned
+        tubeCount++;    
+        textRecieved.text = (tubeCount).ToString();
+    }
+
+    //----------------
+    public void TaskOnClick()
+    {
+
+    }
+
+
 }
--- XRCP_VR/Assets/Activities_XRCP/GroupProjectTemplate/Scripts/TerminalScripts/TubeSpawner.cs
+++ XRCP_VR/Assets/Activities_XRCP/GroupProjectTemplate/Scripts/TerminalScripts/TubeSpawner.cs
@@ -4,43 +4,68 @@ using UnityEngine;
 
 public class TubeSpawner : MonoBehaviour
 {
+    [Header("Tube Reference")] 
     [SerializeField] Transform startPosition;
-    ComputerUI_Controller ui;
-    [SerializeField] GameObject obj;
     [SerializeField] GameObject prefab;
 
-    public bool canSpawnTube = true;
+    [Header("UI")]
+    [SerializeField] GameObject obj;
+     ComputerUI_Controller ui, uiTest;
+
+    [Header("Output")]
+    public bool canSpawnTube = false;
     public int tubeCount = 0;
     //--------------------
     void Start()
     {
         ui = obj.GetComponent<ComputerUI_Controller>();
-        
+        tubeCount = 0;
+
+        //Debug.Log(ui);
     }
 
     //---------------------
     void Update()
     {
         //------------Debugging using UI--------------------
-        if (ui.sliderValue == 1 && canSpawnTube)
-        {
-            Instantiate(prefab, startPosition.transform.position, Quaternion.Euler(-90f, 0f, 0f));
+        //if (ui.sliderValue == 1 && canSpawnTube)
+        //{
+        //    Instantiate(prefab, startPosition.transform.position, Quaternion.Euler(-90f, 0f, 0f));
 
-            canSpawnTube = false;
-            ui.sliderValue = 0;
-            canSpawnTube = true;
-            tubeCount++;
-        }
+        //    canSpawnTube = false;
+        //    ui.sliderValue = 0;
+        //    canSpawnTube = true;
+        //    tubeCount++;
+        //}
+
+
     }
+
+    public void Test()
+    {
+   
+    }
+
+
 
     public void spawnTube()
     {
-        obj = Instantiate(prefab, startPosition.transform.position, Quaternion.Euler(-90f, 0f, 0f));
-        var component = obj.AddComponent<CustomComponent>();
-        component.uniqueValue = tubeCount;
+        uiTest = FindObjectOfType<ComputerUI_Controller>();
+
+        
+        //obj = Instantiate(prefab, startPosition.transform.position, Quaternion.Euler(-90f, 0f, 0f));
+        //var component = obj.AddComponent<CustomComponent>();
+        //component.uniqueValue = tubeCount;
+
+        Instantiate(prefab, startPosition.transform.position, Quaternion.Euler(-90f, 0f, 0f));
+        //ui.UpdateTubeCount();
         tubeCount += 1;
-        Debug.Log("tube created with unique id: " + obj.GetComponent<CustomComponent>().uniqueValue);
+        //Debug.Log("tube created with unique id: " + obj.GetComponent<CustomComponent>().uniqueValue);
         Debug.Log("tube count is " + tubeCount);
+
+        uiTest.UpdateTubeCount();
+
+ 
     }
 
 
