9f78d97d756e7d96b84daa48ef9e41c024887ee5
Sarah Selby <57141798+sarahselby@users.noreply.github.com>
2023-03-21 00:29:39 +0000

    fixed game loop

--- XRCP_VR/Assets/Activities_XRCP/GroupProjectTemplate/Scripts/TerminalScripts/TubeSpawnerMinistry.cs
+++ XRCP_VR/Assets/Activities_XRCP/GroupProjectTemplate/Scripts/TerminalScripts/TubeSpawnerMinistry.cs
@@ -4,41 +4,72 @@ using UnityEngine;
 
 public class TubeSpawnerMinistry : MonoBehaviour
 {
+    [Header("Tube Reference")] 
     [SerializeField] Transform startPosition;
-    ComputerUI_Controller ui;
-    [SerializeField] GameObject obj;
     [SerializeField] GameObject prefab;
 
-    public bool canSpawnTube = true;
+    [Header("UI")]
+    [SerializeField] GameObject obj;
+    ComputerUI_Controller ui, uiTest;
+
+    [Header("Output")]
+    public bool canSpawnTube = false;
     public int tubeCount = 0;
+
+
+    static PlaySound playSound;
+    static bool canTriggerSound = false;
+
+
+
+
     //--------------------
     void Start()
     {
         ui = obj.GetComponent<ComputerUI_Controller>();
-        
-    }
-    public void spawnTubeMinistry()
-    {
-        obj = Instantiate(prefab, startPosition.transform.position, Quaternion.Euler(-90f, 0f, 0f));
-        var component = obj.AddComponent<CustomComponent>();
-        component.uniqueValue = tubeCount;
-        tubeCount += 1;
-        Debug.Log("tube created with unique id: " + obj.GetComponent<CustomComponent>().uniqueValue);
-        Debug.Log("tube count is " + tubeCount);
+        playSound = gameObject.GetComponent<PlaySound>();
+
+        tubeCount = 0;
     }
 
+    private void Update()
+    {
+        //if (canTriggerSound)
+        //{
+        //    playSound.TriggerSound();
+        //}
+
+
+
+    }
+
+    public void AudioTrigger()
+    {
+   
+  
+    }
 
     //---------------------
-    void Update()
+    public void spawnTubeMinistry()
     {
-        if (ui.sliderValue == 1 && canSpawnTube)
-        {
-            Instantiate(prefab, startPosition.transform.position, Quaternion.Euler(-90f, 0f, 0f));
+        uiTest = FindObjectOfType<ComputerUI_Controller>();
+        playSound.TriggerSound();
+        canTriggerSound = true;
 
-            canSpawnTube = false;
-        }
+        //obj = Instantiate(prefab, startPosition.transform.position, Quaternion.Euler(-90f, 0f, 0f));
+        //var component = obj.AddComponent<CustomComponent>();
+        //component.uniqueValue = tubeCount;
+
+        Instantiate(prefab, startPosition.transform.position, Quaternion.Euler(-90f, 0f, 0f));
+        //ui.UpdateTubeCount();
+        tubeCount += 1;
+        //Debug.Log("tube created with unique id: " + obj.GetComponent<CustomComponent>().uniqueValue);
+        Debug.Log("tube count is " + tubeCount);
+
+        uiTest.UpdateTubeCount();
+
+        
     }
-
 
 
 
